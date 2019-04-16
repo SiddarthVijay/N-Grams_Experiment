b364b3ed0f36ca3c81b0e76bae09e02f8882c727
Siddarth Vijay <vsiddarthvijay@gmail.com>
2019-04-16 15:32:28 +0530

    Setupping up corpus inputs

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+
+from experiment_answers import Answers
+from data import Articles
 
 app = Flask(__name__)
 
@@ -20,6 +22,11 @@ class RegisterForm(Form):
     confirm = PasswordField('Confirm Password')
 
 
+class ChooseExperiment(Form):
+    choice = StringField('Choice', [validators.Length(min=1, max=5)])
+# class ExperimentFormA(Form):
+
+
 @app.route('/')
 def introduction():
     return render_template('introduction.html')
@@ -35,9 +42,34 @@ def objective():
     return render_template('objective.html')
 
 
-@app.route('/experiment')
+@app.route('/experiment', methods=['GET', 'POST'])
 def experiment():
-    return render_template('experiment.html')
+    form = ChooseExperiment(request.form)
+    if request.method == 'POST' and form.validate():
+        choice = form.choice.data
+
+        if choice == 'A':
+            flash('You have chosen Corpus A', 'success')
+            return redirect(url_for('experiment_corpusA'))
+        elif choice == 'B':
+            flash('You have chosen Corpus B', 'success')
+            return redirect(url_for('experiment_corpusB'))
+        else:
+            flash('This Corpus does not exist', 'success')
+            return redirect(url_for('experiment'))
+    return render_template('experiment.html', form=form)
+
+
+@app.route('/experiment_corpusA', methods=['GET', 'POST'])
+def experiment_corpusA():
+    form = ChooseExperiment(request.form)
+    return render_template('experiment_corpusA.html', form=form)
+
+
+@app.route('/experiment_corpusB', methods=['GET', 'POST'])
+def experiment_courpusB():
+    form = ChooseExperiment(request.form)
+    return render_template('experiment_corpusB.html', form=form)
 
 
 @app.route('/quiz')
@@ -74,4 +106,5 @@ def register():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'bZ\x85\xb2\xfc1$\xe6\n\xa1\xc0\xce\xdd\x9f\x815\xc0\xe4\xac\xc6\xfc\x0e\xa9\xa0V'
     app.run(debug=True)
